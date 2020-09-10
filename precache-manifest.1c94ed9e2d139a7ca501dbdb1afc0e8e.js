3f271157aad493167ce27c298d9e382441472459
MuhammadHaris2 <mhari8339@gmail.com>
2020-09-10 16:51:39 +0500

    Updates

---
+++ precache-manifest.1c94ed9e2d139a7ca501dbdb1afc0e8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad2572e6aeed31d99f97ccbce4d21e5",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "f5ca8106c27a74c7ba95",
+    "url": "/netflix-clone/static/css/main.69c68543.chunk.css"
+  },
+  {
+    "revision": "9d3611a74dc57eac6333",
+    "url": "/netflix-clone/static/js/2.a8ed09ab.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.a8ed09ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ca8106c27a74c7ba95",
+    "url": "/netflix-clone/static/js/main.677c2d00.chunk.js"
+  },
+  {
+    "revision": "65f55783e4d028a2e4f7",
+    "url": "/netflix-clone/static/js/runtime-main.f7b37b76.js"
+  }
+]);
